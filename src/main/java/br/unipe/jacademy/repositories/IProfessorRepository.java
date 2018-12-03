754a0fb5f4314506360b04011ffef8ecd2f0f4c0
josehenriquefs <zhenrique_@hotmail.com>
2018-12-03 15:46:30 -0200

    Ajustes em Professor

--- src/main/java/br/unipe/jacademy/repositories/IProfessorRepository.java
+++ src/main/java/br/unipe/jacademy/repositories/IProfessorRepository.java
@@ -2,6 +2,12 @@ package br.unipe.jacademy.repositories;
 
 import br.unipe.jacademy.entities.AlunoEntity;
 import br.unipe.jacademy.entities.ProfessorEntity;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IProfessorRepository extends IGenericRepository<ProfessorEntity>{
+
+    @Query("select s from ProfessorEntity s where s.nome like %?1%")
+    List<ProfessorEntity> findProfessoresByName(String nome);
 }
